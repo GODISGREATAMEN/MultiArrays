c8591977688521c88595fa3664b72a70d0b33848
GODISGREATAMEN <mixi271020@gmail.com>
2023-11-17 22:34:52 +0200

    Modify 'Main'

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -4,47 +4,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final int WINNING_NUMBER = 34;
         Scanner scan = new Scanner(System.in);
         int userNumber;
-        boolean isWinner = false;
         int[][] lotteryNumbers = new int[][]{
                 {29, 16, 50, 37},
                 {43, 17, 88, 99},
                 {21, 34, 51, 8},
                 {49, 91, 27, 76}};
+        final int WINNING_NUMBER = lotteryNumbers[2][1];
 
-        System.out.println("Hi!\nEnter the number from 0 to 100 you expect to be the winning one:");
+        System.out.println("Thank you for your purchase!");
+        System.out.println("You have the following ticket:");
+        System.out.println("---------------------");
+        for (int[] lotteryNumber : lotteryNumbers) {
+            for (int i : lotteryNumber) {
+                System.out.print(i + "    ");
+            }
+            System.out.println();
+        }
+        System.out.println("---------------------\nYou need to choose one number!");
+        System.out.println("Caution: Only one number can be the winning one\nEnter your number:");
 
         if (scan.hasNextInt()) {
             userNumber = scan.nextInt();
-            for (int i = 0; i < lotteryNumbers.length && !isWinner; i++) {
-                for (int j = 0; j < lotteryNumbers[i].length; j++) {
-                    if (userNumber == WINNING_NUMBER) {
-                        isWinner = true;
-                        break;
-                    }
-                }
-            }
-            if (!isWinner) {
-                System.out.println("Unfortunately, you lost!");
-                System.out.println("The winning number was: " + WINNING_NUMBER + "\n");
+            if (userNumber == WINNING_NUMBER) {
+                System.out.println("You're lucky today, you won!!!");
             } else {
-                System.out.println("You're lucky today!!!\n");
+                System.out.println("Unfortunately, you lost!");
+                System.out.println("The winning number was: " + WINNING_NUMBER);
+
             }
-
-            System.out.println("The ticket was:\n---------------------");
-
-            for (int[] lotteryNumber : lotteryNumbers) {
-                for (int i : lotteryNumber) {
-                    System.out.print(i + "    ");
-                }
-                System.out.println();
-            }
-
-            System.out.println("---------------------");
         } else {
-            System.out.println("You entered something wrong!");
+            System.out.println("You entered something different!\nTry again!!!");
         }
     }
 }
